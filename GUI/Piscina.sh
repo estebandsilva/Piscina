7ad973183109c025653eb30d5beee8ef8a828a6a
estebandsilva <137509594+estebandsilva@users.noreply.github.com>
2024-09-06 15:00:58 +0200

    Update Piscina.sh

--- GUI/Piscina.sh
+++ GUI/Piscina.sh
@@ -13,8 +13,11 @@ if [ -d "$ruta_directorio" ]; then
     # ... añade aquí la rutina que deseas ejecutar
     cd $ruta_directorio
     #cd /home/mequonic/Desktop/Piscina
-    #git pull
+    #Reset all the chages done of the git
     git reset --hard
+    #Pull the latest changes of the git
+    git pull
+    #Goes to the main branch
     git checkout main
     cd GUI
     chmod +x $python_file_executable
